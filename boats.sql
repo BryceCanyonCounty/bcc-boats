19a7540d1b1b4ac92658ae06c6208eff797504f6
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-08-15 20:05:07 -0700

    Update boats.sql
    - forgot to change null value in alter table entries

--- boats.sql
+++ boats.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS `boats` (
   `condition` int(11) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `boats` ADD COLUMN IF NOT EXISTS (`fuel` INT(11) NOT NULL);
-ALTER TABLE `boats` ADD COLUMN IF NOT EXISTS (`condition` INT(11) NOT NULL);
+ALTER TABLE `boats` ADD COLUMN IF NOT EXISTS (`fuel` INT(11) NULL);
+ALTER TABLE `boats` ADD COLUMN IF NOT EXISTS (`condition` INT(11) NULL);
 
 INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`) VALUES ('portable_canoe', 'Portable Canoe', 1, 1, 'item_standard', 1, 'Bryce Canyon Canoes')
 ON DUPLICATE KEY UPDATE `item`='portable_canoe', `label`='Portable Canoe', `limit`=1, `can_remove`=1, `type`='item_standard', `usable`=1, `desc`='Bryce Canyon Canoes';
