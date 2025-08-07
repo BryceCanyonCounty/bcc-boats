dbf22ab2127b9874f4183565e5143826a4874c7a
Apollyon <37031311+JusCampin@users.noreply.github.com>
2025-08-07 14:16:34 -0700

    Create translations.lua

---
+++ configs/translations.lua
@@ -0,0 +1,15 @@
+-- Main Boat Shop Menu Translations
+Translations = {
+    noPersonalBoat = 'No Boats, head to the Shop!',
+    renamePersonalBoat = 'Rename',
+    spawnPersonalBoat = 'Launch',
+    sellPersonalBoat = 'Sell',
+    sellConfirmation = 'Are you sure you want to sell?',
+    sellConfirmationButton = 'Sell',
+    sellConfirmationCancelButton = 'Cancel',
+    boatButton = 'My Boats',
+    traderButton = 'Boat Shop',
+    saveButton = 'Save',
+    rotateText = 'Rotate',
+    cancelButton = 'Cancel',
+}
